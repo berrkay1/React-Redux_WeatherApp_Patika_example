d329614c18b19ee76e7f787e6182434ab84d0ce6
BERKAY GÜL <berrkaygul@gmail.com>
2022-06-03 20:25:48 +0300

    commit

--- src/components/CityInput.jsx
+++ src/components/CityInput.jsx
@@ -24,7 +24,7 @@ function CityInput() {
                         <option key={index} value={item.name} >{item.name}</option>
                     ))}
 
-
+                    
                 </select>
             </form>
 
